bfaef020d3540344e1e702059abb782c82f92e71
Vera <veraagiang@gmail.com>
2026-08-05 11:35:29 +0100

    Update favicon and internal link behavior

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://builtbyvera.com',
   output: 'static',
-  trailingSlash: 'never',
+  trailingSlash: 'ignore',
 });
